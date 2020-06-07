7c874477aa54d89036f247b3ef00a02ad7963fae
Hans Fangohr grey laptop <hans.fangohr@xfel.eu>
2020-06-07 07:43:58 +0200

    updated Sun Jun  7 07:43:57 CEST 2020

--- ipynb/coronavirus/coronavirus.py
+++ ipynb/coronavirus/coronavirus.py
@@ -277,7 +277,7 @@ def fetch_data_germany(include_last_day=True):
 
     """
 
-    datasource = "https://opendata.arcgis.com/datasets/dd4580c810204019a7b8eb3e0b329dd6_0.csv"
+    # outdated: datasource = "https://opendata.arcgis.com/datasets/dd4580c810204019a7b8eb3e0b329dd6_0.csv"
     datasource = "https://www.arcgis.com/sharing/rest/content/items/f10774f1c63e40168479a1feb6c7ca74/data"
     t0 = time.time()
     print(f"Please be patient - downloading data from {datasource} ...")
